0cd80fcddf1005a5d16f61a2c5e326da60771b05
andrew-nam <168607546+andyrew-nam@users.noreply.github.com>
2025-07-25 23:29:37 -0400

    Update neural_network.v

--- Main/neural_network.v
+++ Main/neural_network.v
@@ -1,1 +1,13 @@
+`timescale 1ns/1ns
 
+module spike_neuron_model (clk, n_in1, n_in2, n_in3, w1, w2, w3, n_out);
+  // Neuron Input Declarations
+  input wire clk;
+  input wire [2:0] w1;
+  input wire [2:0] w2;
+  input wire [2:0] w3;
+  input wire n_in1;
+  input wire n_in2;
+  input wire n_in3;
+
+  
